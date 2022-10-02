2ce775dcd8293fffa56717aee0041f514974940d
jimbaemon <jimbae@kakao.com>
2022-10-03 01:04:03 +0900

    feat: 사용자가 잘못된 값을 입력하면 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료된다.

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,18 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+    private final List<Integer> numbers;
+
+    public Player(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() > 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
