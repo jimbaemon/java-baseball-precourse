64c3ecd52cd8d5916735dfdc9b6f8d8c98cfa136
jimbaemon <jimbae@kakao.com>
2022-10-03 04:11:34 +0900

    fix: Randoms 의 pickNumberInRange 사용하도록 변경.

--- src/main/java/baseball/model/RandomNumberGenerator.java
+++ src/main/java/baseball/model/RandomNumberGenerator.java
@@ -1,11 +1,18 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandomNumberGenerator implements NumberGenerator{
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1,9, 3);
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < 3) {
+            numbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return new ArrayList<>(numbers);
     }
 }
